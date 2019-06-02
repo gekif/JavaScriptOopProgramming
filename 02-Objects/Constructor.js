985348504330a2d33b60e50d54373949a2b7ad39
Gekif <dzulfikar.maulana@gmail.com>
2019-06-02 13:56:00 +0700

    02-Objects: Constructor

---
+++ 02-Objects/Constructor.js
@@ -0,0 +1,25 @@
+// Factory Function
+function createCircle(radius) {
+    return {
+        radius,
+        draw() {
+            console.log('draw');
+        }
+    }
+}
+
+// const circle = createCircle(1);
+
+
+// Constructor Function
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function () {
+        console.log('draw');
+    }
+}
+
+// const another = new Circle(1);
+const another = new Circle();
+
+console.log(another.draw());
